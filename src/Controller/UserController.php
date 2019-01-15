9f46d32bd4375ac3b8041e899bdfe18ead05cf1b
Elie Boury <eboury@info-301-14x.iut.bx1>
2019-01-15 14:10:26 +0100

    qq avances

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Pays;
 use App\Entity\Abonne;
 use Symfony\Component\Form\Forms;
 use phpDocumentor\Reflection\Types\Boolean;
+use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;   
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -29,33 +30,118 @@ class UserController extends AbstractController
         ]);
     }
 
+    //TODO Afficher pays
+    //TODO Modify
     /**
      * @Route("/enregistrer", name="user_register")
      * @Route("/modify", name="user_modify")
      */
-    public function register(Abonne $user = null, Request $request, ObjectManager $manager,
-    UserPasswordEncoderInterface $encoder)
-    {
-        if($user == null) {
+    public function register(
+        Abonne $user = null,
+        Request $request,
+        ObjectManager $manager,
+        UserPasswordEncoderInterface $encoder
+    ) {
+        if ($user == null) {
             $user = new Abonne();
         }
-        
+
         $form = $this->createFormBuilder($user)
-                    ->add('nomAbonne')
-                    ->add('prenomAbonne')
-                    ->add('email')
-                    ->add('login')
-                    ->add('adresse')
-                    ->add('codePostal')
-                    ->add('ville')
-                    //->add('codePays')
-                    ->add('password', PasswordType::class)
-                    ->add('confirm_password', PasswordType::class)
-                    ->getForm();
+            ->add('nomAbonne', null, [
+                'label' => 'Nom*',
+                'attr' => [
+                    'placeholder' => 'ex: Dupont',
+                    'required' => true
+                ]
+            ])
+            ->add('prenomAbonne', null, [
+                'label' => 'Prenom*',
+                'attr' => [
+                    'placeholder' => 'ex: Albert',
+                    'required' => true
+                ]
+            ])
+            ->add('email', null, [
+                'label' => 'Email*',
+                'attr' => [
+                    'placeholder' => 'ex: email@email.fr',
+                    'required' => true
+                ]
+            ])
+            ->add('login', null, [
+                'label' => 'Identifiant*',
+                'attr' => [
+                    'placeholder' => 'ex: DupontDu33',
+                    'required' => true
+                ]
+            ])
+            ->add('adresse', null, [
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'ex: 564 Avenue des ponts',
+                    'required' => false
+                ]
+            ])
+            ->add('codePostal', null, [
+                'label' => 'Code Postal',
+                'attr' => [
+                    'placeholder' => 'ex: 66000',
+                    'required' => false
+                ]
+            ])
+            ->add('ville', null, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'ex: Perpignan',
+                    'required' => false
+                ]
+            ])
+
+            /*->add('codePays', Pays::class, [
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'ex: France',
+                    'required' => false
+                ]
+            ])*/
+
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe*',
+                'attr' => [
+                    'placeholder' => 'Mot de passe',
+                    'required' => true
+                ]
+            ])
+            ->add('confirm_password', PasswordType::class, [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer le mot de passe',
+                        'required' => true
+                    ]
+            ])
+            ->getForm();
+
+        //if ($user == null) {
+            /*$form->add('password', PasswordType::class, [
+                'label' => 'Mot de passe*',
+                'attr' => [
+                    'placeholder' => 'Mot de passe',
+                    'required' => true
+                ]
+            ])
+                ->add('confirm_password', PasswordType::class, [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmer le mot de passe',
+                        'required' => true
+                    ]
+            ])
+            ->getForm();*/
+        //}
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $hash = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hash);
 
@@ -65,9 +151,9 @@ class UserController extends AbstractController
             return $this->redirectToRoute('user_login');
         }
 
-    return $this->render('user\register.html.twig', [
-        'formRegister' => $form->createView()
-    ]);
+        return $this->render('user\register.html.twig', [
+            'formRegister' => $form->createView()
+        ]);
     }
 
     /**
@@ -83,5 +169,7 @@ class UserController extends AbstractController
     /**
      * @Route("/logout", name="user_logout")
      */
-    public function logout() {}
+    public function logout()
+    {
+    }
 }
